e48972828a6af491d9f9ee9d5733ee97d61c6ed8
Wajeha Fatima <maharfatima90@gmail.com>
2025-02-17 03:12:12 -0800

    up

---
+++ lib/src/view/auth_view/login with phoneView/verify_codeView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class VerifyCodeview extends StatefulWidget {
+  const VerifyCodeview({super.key});
+
+  @override
+  State<VerifyCodeview> createState() => _VerifyCodeviewState();
+}
+
+class _VerifyCodeviewState extends State<VerifyCodeview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
