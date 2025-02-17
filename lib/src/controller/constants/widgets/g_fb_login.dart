e678ec0156b9f6e89dc7ecba029a6858914fd349
Wajeha Fatima <maharfatima90@gmail.com>
2025-02-17 03:19:52 -0800

    uppp

--- lib/src/controller/constants/widgets/g_fb_login.dart
+++ lib/src/controller/constants/widgets/g_fb_login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:newproject/src/view/auth_view/login%20with%20phoneView/logIn_with_phone.dart';
 
 import '../../assets/colors/appColors.dart';
 import '../../assets/images/appImages.dart';
@@ -39,13 +40,18 @@ class _FbGoogleState extends State<FbGoogle> {
               borderRadius: BorderRadius.circular(12.r)
 
           ),
-          child: Row(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(Appimages.fbLogo,width: 24.w,height: 24.h,),
-              SizedBox(width: 10.w,),
-              Text('Facebook',style: GoogleFonts.roboto(color: Colors.white,fontWeight: FontWeight.w400,fontSize: 14.sp),),
+          child: InkWell(onTap: (){
+            Navigator.pushReplacement(context, MaterialPageRoute(
+                builder: (context) => LoginWithPhone()));
+          },
+            child: Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(Appimages.fbLogo,width: 24.w,height: 24.h,),
+                SizedBox(width: 10.w,),
+                Text('Phone',style: GoogleFonts.roboto(color: Colors.white,fontWeight: FontWeight.w400,fontSize: 14.sp),),
 
-            ],
+              ],
+            ),
           ),),
 
       ],);
--- lib/src/view/auth_view/login with phoneView/verify_codeView.dart
+++ lib/src/view/auth_view/login with phoneView/verify_codeView.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../controller/assets/colors/appColors.dart';
 class VerifyCodeview extends StatefulWidget {
   const VerifyCodeview({super.key});
 
@@ -9,6 +13,28 @@ class VerifyCodeview extends StatefulWidget {
 class _VerifyCodeviewState extends State<VerifyCodeview> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(backgroundColor: Colors.white,
+        body:
+        Padding(
+            padding: const EdgeInsets.only(top: 50),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8,top: 30),
+                    child: Text(
+                      'mtodo Logo',
+                      style: GoogleFonts.jost(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20.sp,
+                          color: AppColors.darkBlue
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30.h),
+
+
+                ]))
+    );
   }
 }
