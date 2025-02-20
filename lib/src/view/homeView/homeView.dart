b6e0727fef4c687a9dc4339a485e0aa2fa881c97
Wajeha Fatima <maharfatima90@gmail.com>
2025-02-19 23:04:16 -0800

    upppp

--- lib/src/view/homeView/homeView.dart
+++ lib/src/view/homeView/homeView.dart
@@ -1,3 +1,4 @@
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -20,174 +21,205 @@ class Homeview extends StatefulWidget {
 }
 
 class _HomeviewState extends State<Homeview> {
-  final auth= FirebaseAuth.instance;
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.lightBlue,
+        backgroundColor: AppColors.lightBlue,
 
 
-      drawer:  Drawer(
-        child: Container(color: Colors.white,
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                ),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: AssetImage(Appimages.profie),
-                    ),
-                    SizedBox(width: 10.h),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Hey Wajeha',
-                          style: TextStyle(
-                              color: AppColors.darkBlue,
-                              fontSize: 14.h,
-                              fontWeight: FontWeight.w400
-                          ),
-                        ),
-                        Text(
-                          'wajehafatima92@gmail.com',
-                          style: TextStyle(
-                              color: AppColors.Grey,
-                              fontSize: 10.h,
-                              fontWeight: FontWeight.w400
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              CustomListTile(icon: Icons.edit, title: 'Edit profile'),
-              CustomListTile(icon: Icons.edit_calendar_outlined, title: 'Daily tasks'),
-              CustomListTile(icon: Icons.star, title: 'Important Tasks'),
-              CustomListTile(icon:Icons.done_all, title: 'Done tasks'),
-              CustomListTile(icon: Icons.logout, title: 'Logout',onTap: (){
-               auth.signOut().then((value){
-                 Navigator.pushReplacement(context, MaterialPageRoute(
-                     builder: (context) => Loginview())).onError((error,stackTrace){
-                       Utils().toastMessage(error.toString());
-                 });
-               });
-              },)
-            ],
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            // Gradient Background
-            Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    AppColors.darkBlue,
-                    Colors.white,
-                  ],
-                ),
-              ),
-            ),
-
-            // Content
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: [SizedBox(height: 20.h,),
-                  // First Row
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        drawer: Drawer(
+          child: Container(color: Colors.white,
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
                     children: [
-                      Builder(builder: (context){
-                        return  IconButton(onPressed: (){
-                          Scaffold.of(context).openDrawer();
-                        }, icon: Icon(Icons.menu));
-                      }),
-                      Text(
-                        'Mtodo Logo',
-                        style: GoogleFonts.jost(
-                          color: Colors.white,
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
                       CircleAvatar(
                         backgroundImage: AssetImage(Appimages.profie),
                       ),
-                    ],
-                  ),
-
-                  // Add spacing below the Row
-                  SizedBox(height: 30.h),
-
-                  // RichTextHome Section
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Richtexthome(),
-
-                      SizedBox(height: 10.h),
-                      Text(
-                        'Saturday, August 15, 2024',
-                        style: GoogleFonts.jost(
-                          fontSize: 12.sp,
-                          fontWeight: FontWeight.w500,
-                        ),
+                      SizedBox(width: 10.h),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Hey Wajeha',
+                            style: TextStyle(
+                                color: AppColors.darkBlue,
+                                fontSize: 14.h,
+                                fontWeight: FontWeight.w400
+                            ),
+                          ),
+                          Text(
+                            'wajehafatima92@gmail.com',
+                            style: TextStyle(
+                                color: AppColors.Grey,
+                                fontSize: 10.h,
+                                fontWeight: FontWeight.w400
+                            ),
+                          ),
+                        ],
                       ),
-
-SizedBox(height: 10.h,),
-                      SearchBar(),
-
-          ]
-        ),
-                  SizedBox(height: 30.h,),
-                  Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Timebuttons(text: 'Today', onPressed: (){
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Insertdataview()),
-                        );
-                      }),
-                      Timebuttons(text: 'Week', onPressed: (){}),
-                      Timebuttons(text: 'Monthly', onPressed: (){})
                     ],
                   ),
-                  SizedBox(height: 30.h),
-                  Wrap(
-                    children: [
-                      Taskbox(backgroundColor: Color(0xff2A8899), imagePath: Appimages.school,
-                          title: 'School', countText: '7', titleColor: Colors.white),
-                      Taskbox(backgroundColor: Color(0xff5EB0D2), imagePath: Appimages.work,
-                          title: 'Work', countText: '6', titleColor:Colors.white),
-                      Taskbox(backgroundColor: Color(0xffBE8972), imagePath: Appimages.shop, title: 'Shop',
-                          countText: '3', titleColor: Colors.white),
-                      Taskbox(backgroundColor: Color(0xff646FD4), imagePath: Appimages.read, title: 'Read', countText: '4', titleColor: Colors.white),
-                      Taskbox(backgroundColor: Color(0xff83BC74), imagePath: Appimages.health, title:'Workout', countText: '1', titleColor: Colors.white)
-                      ,
-                      Taskbox(backgroundColor: Color(0xffffe8e8), imagePath: Appimages.add, title: '', countText: '', titleColor: Colors.white)],
-
+                ),
+                CustomListTile(icon: Icons.edit, title: 'Edit profile'),
+                CustomListTile(
+                    icon: Icons.edit_calendar_outlined, title: 'Daily tasks'),
+                CustomListTile(icon: Icons.star, title: 'Important Tasks'),
+                CustomListTile(icon: Icons.done_all, title: 'Done tasks'),
+                CustomListTile(icon: Icons.logout, title: 'Logout', onTap: () {
+                  auth.signOut().then((value) {
+                    Navigator.pushReplacement(context, MaterialPageRoute(
+                        builder: (context) => Loginview())).onError((error,
+                        stackTrace) {
+                      Utils().toastMessage(error.toString());
+                    });
+                  });
+                },)
+              ],
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Stack(
+              children: [
+                // Gradient Background
+                Container(
+                  height: MediaQuery
+                      .of(context)
+                      .size
+                      .height,
+                  width: MediaQuery
+                      .of(context)
+                      .size
+                      .width,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        AppColors.darkBlue,
+                        Colors.white,
+                      ],
+                    ),
                   ),
+                ),
+
+                // Content
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                      children: [SizedBox(height: 20.h,),
+                        // First Row
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Builder(builder: (context) {
+                              return IconButton(onPressed: () {
+                                Scaffold.of(context).openDrawer();
+                              }, icon: Icon(Icons.menu));
+                            }),
+                            Text(
+                              'Mtodo Logo',
+                              style: GoogleFonts.jost(
+                                color: Colors.white,
+                                fontSize: 20.sp,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            CircleAvatar(
+                              backgroundImage: AssetImage(Appimages.profie),
+                            ),
+                          ],
+                        ),
+
+                        // Add spacing below the Row
+                        SizedBox(height: 30.h),
+
+                        // RichTextHome Section
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Richtexthome(),
+
+                              SizedBox(height: 10.h),
+                              Text(
+                                'Saturday, August 15, 2024',
+                                style: GoogleFonts.jost(
+                                  fontSize: 12.sp,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+
+                              SizedBox(height: 10.h,),
+                              SearchBar(),
+
+                            ]
+                        ),
+                        SizedBox(height: 30.h,),
+                        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Timebuttons(text: 'Today', onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Insertdataview()),
+                              );
+                            }),
+                            Timebuttons(text: 'Week', onPressed: () {}),
+                            Timebuttons(text: 'Monthly', onPressed: () {})
+                          ],
+                        ),
+                        SizedBox(height: 30.h),
+                        Wrap(
+                          children: [
+                            Taskbox(backgroundColor: Color(0xff2A8899),
+                                imagePath: Appimages.school,
+                                title: 'School',
+                                countText: '7',
+                                titleColor: Colors.white),
+                            Taskbox(backgroundColor: Color(0xff5EB0D2),
+                                imagePath: Appimages.work,
+                                title: 'Work',
+                                countText: '6',
+                                titleColor: Colors.white),
+                            Taskbox(backgroundColor: Color(0xffBE8972),
+                                imagePath: Appimages.shop,
+                                title: 'Shop',
+                                countText: '3',
+                                titleColor: Colors.white),
+                            Taskbox(backgroundColor: Color(0xff646FD4),
+                                imagePath: Appimages.read,
+                                title: 'Read',
+                                countText: '4',
+                                titleColor: Colors.white),
+                            Taskbox(backgroundColor: Color(0xff83BC74),
+                                imagePath: Appimages.health,
+                                title: 'Workout',
+                                countText: '1',
+                                titleColor: Colors.white)
+                            ,
+                            Taskbox(backgroundColor: Color(0xffffe8e8),
+                                imagePath: Appimages.add,
+                                title: '',
+                                countText: '',
+                                titleColor: Colors.white)
+                          ],
+
+                        ),
 
 
-
-      ]),
-    )
-          ]),
-      )
+                      ]),
+                )
+              ]),
+        )
     );
   }
 }
